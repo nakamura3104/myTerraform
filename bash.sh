05daa0a1db1a303d21d0b0c33b365e2d50f360b7
nakamura3104 <60680996+nakamura3104@users.noreply.github.com>
2023-03-23 15:02:19 +0900

    Update and rename bash.sh to shell_for_azure.sh

--- bash.sh
+++
@@ -1,5 +0,0 @@
-# Git branch in the prompt
-parse_git_branch() {
-   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-export PS1="\u@\h:\w\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
---
+++ shell_for_azure.sh
@@ -0,0 +1,19 @@
+
+################################################3
+#
+# Git branch in the CloudShell prompt
+#
+parse_git_branch() {
+   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+export PS1="\u@\h:\w\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+
+################################################3
+#
+# Get Credential
+#
+export ARM_CLIENT_ID=$(az account show --query 'user.name' -o tsv)
+export ARM_CLIENT_SECRET=$ACCESS_TOKEN
+export ARM_TENANT_ID=$(az account show --query 'tenantId' -o tsv)
+export ARM_SUBSCRIPTION_ID=$(az account show --query 'id' -o tsv)
