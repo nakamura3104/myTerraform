40f676a37ad276004cb3417e3cd13d7a14c5b062
nakamura3104 <60680996+nakamura3104@users.noreply.github.com>
2023-04-20 13:43:26 +0900

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -7,24 +7,36 @@ def lambda_handler(event, context):
     # HTTPメソッドを取得
     http_method = event['httpMethod']
 
-    # メソッドに応じたメッセージを作成
+    # メソッドに応じたレスポンスを作成
     if http_method == 'GET':
-        message = "Hello from Lambda and API Gateway with a GET request!"
+        response = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "message": "Hello from Lambda and API Gateway with a GET request!",
+                "event": event_str
+            })
+        }
     elif http_method == 'PUT':
-        message = "Hello from Lambda and API Gateway with a PUT request!"
+        response = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "text/plain"
+            },
+            "body": "Hello from Lambda and API Gateway with a PUT request!"
+        }
     else:
-        message = "Hello from Lambda and API Gateway with an unsupported request!"
-
-    # レスポンスを作成
-    response = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "message": message,
-            "event": event_str
-        })
-    }
+        response = {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "message": "Unsupported method.",
+                "event": event_str
+            })
+        }
 
     return response
