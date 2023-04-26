2d2ef19253a833a3e620447984f8204a5b79f262
nakamura3104 <60680996+nakamura3104@users.noreply.github.com>
2023-04-26 23:32:01 +0900

    Update custom_domain_web_app.tf

--- custom_domain_web_app.tf
+++ custom_domain_web_app.tf
@@ -23,3 +23,30 @@ resource "azurerm_app_service_custom_hostname_binding" "example" {
 resource "azurerm_app_service_certificate" "example" {
   # ... SSL証明書の設定 ...
 }
+
+
+#### gen 04/26
+data "azurerm_key_vault" "example" {
+  name                = "<your_key_vault_name>"
+  resource_group_name = "<your_key_vault_resource_group>"
+}
+
+data "azurerm_key_vault_secret" "example_certificate" {
+  name         = "<your_key_vault_certificate_secret_name>"
+  key_vault_id = data.azurerm_key_vault.example.id
+}
+
+resource "azurerm_app_service_certificate" "example" {
+  name                = "example-certificate"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  key_vault_secret_id = data.azurerm_key_vault_secret.example_certificate.id
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "example" {
+  hostname            = "<your_custom_domain>"
+  app_service_name    = azurerm_app_service.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  ssl_state           = "SniEnabled"
+  thumbprint          = azurerm_app_service_certificate.example.thumbprint
+}
